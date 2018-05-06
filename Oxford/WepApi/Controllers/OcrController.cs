795f96bb38ca6183c41352b6222e279f87ef9b09
Slava Agafonov <Agafonov.Slava@hotmail.com>
2018-05-06 14:10:34 -0700

    Update service cache

--- Oxford/WepApi/Controllers/OcrController.cs
+++ Oxford/WepApi/Controllers/OcrController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -15,7 +16,20 @@ namespace WepApi.Controllers
     public class OcrController : Controller
     {
         public static List<RankingAndRelevance.Provider> providers = Ranker.ReadProviders("Provider.tsv"); //191 provider
-        public Dictionary<string, double[]> cuisDictionary = Ranker.LoadData("Filtered.csv");
+        public static Dictionary<string, double[]> cuisDictionary = Ranker.LoadData("Filtered.csv");
+        public static Dictionary<string, CuiEntities> cuiProviderDictionary = LoadProviderCuiEntities();
+
+        public static Dictionary<string, CuiEntities> LoadProviderCuiEntities()
+        {
+            if (cuiProviderDictionary != null && cuiProviderDictionary.Count > 0) return cuiProviderDictionary;
+            Dictionary<string, CuiEntities> cuiEntities = new Dictionary<string, CuiEntities>();
+            foreach (Provider provider in providers)
+            {
+                NLUclient.CuiEntities providerCuiEntities = NuClient.ExtractCuiEntities(provider.Keywords);
+                cuiEntities.Add(provider.PrvIdn, providerCuiEntities);
+            }
+            return cuiEntities;
+        }
 
         //[HttpGet]
         //public string GetText(string url)
@@ -57,16 +71,15 @@ namespace WepApi.Controllers
             List<string> resultWords = OxfordOCR.OcrProgram.ExtractWords(result);
             string patientDescriptionInputText = OxfordOCR.OcrProgram.DisplayWords(resultWords);
 
-            Dictionary<RankingAndRelevance.Similarity, RankingAndRelevance.Provider> cosineSimilarites = 
-                new Dictionary<RankingAndRelevance.Similarity, RankingAndRelevance.Provider>();
-
             NLUclient.CuiEntities patientCuiEntities = NuClient.ExtractCuiEntities(patientDescriptionInputText);
-            foreach (RankingAndRelevance.Provider provider in providers)
+
+            //Parallel.ForEach(providers, provider =>
+            foreach (KeyValuePair<string, CuiEntities> providerCuiEntities in cuiProviderDictionary)
             {
                 try
                 {
-                    NLUclient.CuiEntities providerCuiEntities = NuClient.ExtractCuiEntities(provider.Keywords);
-                    var similar = RankingAndRelevance.Ranker.ExtractSimilarities(providerCuiEntities, patientCuiEntities, cuisDictionary);
+                    //NLUclient.CuiEntities providerCuiEntities = NuClient.ExtractCuiEntities(provider.Keywords);
+                    var similar = RankingAndRelevance.Ranker.ExtractSimilarities(providerCuiEntities.Value, patientCuiEntities, cuisDictionary);
                     List<double> avg = new List<double>();
                     List<string> matches = new List<string>();
                     foreach (var s in similar)
@@ -74,6 +87,7 @@ namespace WepApi.Controllers
                         avg.Add(s.Rank);
                         matches.Add($"{s.ProviderSurfaceForm} : {s.PatientSurfaceForm}");
                     }
+                    var provider = providers.Find(p => p.PrvIdn == providerCuiEntities.Key);
                     provider.Matches = matches;
                     if (avg.Count > 0)
                     {
@@ -91,61 +105,13 @@ namespace WepApi.Controllers
                     Console.WriteLine(e);
                 }
             }
-            
+            //});
+
             providers.Sort();
             providers.Reverse();
             string json = JsonConvert.SerializeObject(providers, Formatting.Indented);
             Request.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
             return json;
         }
-
-        //[EnableCors("CorsPolicy")]
-        //[HttpGet]
-        //public string GetText2(string url2)
-        //{
-        //    if (string.IsNullOrWhiteSpace(url2)) return "url2 cannot be empty";
-        //    string result = OxfordOCR.OcrProgram.MakeAnalysisRequest(url2);
-        //    if (result == "Bad Request") throw new ApplicationException("Bad request");
-
-        //    if (string.IsNullOrWhiteSpace(result))
-        //    {
-        //        throw new ApplicationException("No text on image was recognized");
-        //    }
-        //    List<string> resultWords = OxfordOCR.OcrProgram.ExtractWords(result);
-        //    string patientDescriptionInputText = OxfordOCR.OcrProgram.DisplayWords(resultWords);
-
-        //    List<RankingAndRelevance.Provider> providers = Ranker.ReadProviders("Provider.tsv"); //191 provider
-        //    CuiEntities patientCuiEntities = NuClient.ExtractCuiEntities(patientDescriptionInputText);
-
-        //    foreach (RankingAndRelevance.Provider provider in providers)
-        //    {
-        //        CuiEntities providerCuiEntities = NuClient.ExtractCuiEntities(provider.Keywords);
-        //    }
-
-        //    //string providerDescriptionInputText = "Small-incision phacoemulsification cataract surgery, minor in-office procedures including small eyelid anomalies and chalazion excisions and laser surgical procedures for secondary cataracts, glaucoma and diabetic complications Special Interests General ophthalmology including cataract surgery, glaucoma and diabetic care.";
-        //    //CuiEntities providerCuiEntities = NuClient.ExtractCuiEntities(providerDescriptionInputText);
-        //    //CuiEntities patientCuiEntities = NuClient.ExtractCuiEntities(patientDescriptionInputText);
-
-        //    string json = JsonConvert.SerializeObject(providers, Formatting.Indented);
-        //    return json;
-        //}
-
-        //// POST api/ocr
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/ocr/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/ocr/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
